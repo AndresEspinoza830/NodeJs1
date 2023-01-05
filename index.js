1108c22ed083c3e4dcafcdebdc77b0229d7fdbc3
Andres Espinoza <andrespinozaaf@gmail.com>
2023-01-05 12:31:49 -0500

    Seccion 5 terminada

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const usuarioRoutes = require('./routes/usuarioRoutes.js')
+import express from 'express';
+import usuarioRoutes from './routes/usuarioRoutes.js';
 
 //Crear la app -> Contiene toda la informacion del servidor de express
 const app = express()
 
 //Routing
-app.use('/', usuarioRoutes)      //use Busca todas las rutas que inicien con / , en cambio get busca una sola
+app.use('/auth', usuarioRoutes)      //use Busca todas las rutas que inicien con /auth o cualquier cosa, en cambio get busca una sola
+
+//Carpeta publica
+app.use(express.static('public'));
+
+//Habilitar pug
+app.set('view engine', 'pug')       //set es para agregar configuracion, le decimos que usaremos pug
+app.set('views', './views')         //Le decimos donde estan nuestras vistas, ya no es necesario especificarlo en las rutas
 
 //Definir un puerto y arrancar el proyecto
 const port = 4000;
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,14 +1,11 @@
-const express = require('express');
+import express from 'express';
+import { formularioLogin, formularioRegistro } from '../controllers/usuarioController.js';
+
 const router = express.Router();
 
 // Routing
-router.get('/', (req, res) => {     //req es lo que envias al servidor de node y res es la respuesta del servidor
-    res.json({ msg: 'Hola mundo en express' })          //send devuelve un texto plano , json devuelve un json y render devuelve una vista
-})
-
-router.post('/', (req, res) => {
-    res.json({ msg: 'Respuesta de Tipo Post' })
-})
+router.get('/login', formularioLogin)           //req es lo que envias al servidor de node y res es la respuesta del servidor
+router.get('/registro', formularioRegistro)     //send devuelve un texto plano , json devuelve un json y render devuelve una vista                   
 
 // router.route('/')
 //     .get((req, res) => {
@@ -18,4 +15,4 @@ router.post('/', (req, res) => {
 //         res.json({ msg: 'Respuesta de Tipo Post' })
 //     })
 
-module.exports = router
+export default router
